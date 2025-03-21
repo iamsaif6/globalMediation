ae5cfc3201e2189345eaabd43dc30ad1c9a89f19
Saif Hasan <saifhassan399@gmail.com>
2025-03-21 18:43:41 +0600

    Update - team member added

--- src/components/home/Marque.jsx
+++ src/components/home/Marque.jsx
@@ -3,6 +3,8 @@ import Marquee from 'react-fast-marquee';
 import img1 from '/public/Layer_2.png';
 import img2 from '/public/ISO 27001_2013 1.png';
 import img3 from '/public/image 15.png';
+import img4 from '/public/cyber.png';
+import img5 from '/public/ISO 9001.png';
 import Image from 'next/image';
 
 const Marque = () => {
@@ -20,6 +22,12 @@ const Marque = () => {
         </div>
         <div className="  h-24  mx-[30px] lg:mx-[70px] w-24">
           <Image className="w-full h-full object-contain" src={img2} alt="ISO" />
+        </div>
+        <div className="  h-24  mx-[30px] lg:mx-[70px] w-24">
+          <Image className="w-full h-full object-contain" src={img4} alt="Cyber" />
+        </div>
+        <div className="  h-24  mx-[30px] lg:mx-[70px] w-24">
+          <Image className="w-full h-full object-contain" src={img5} alt="Cyber" />
         </div>
       </Marquee>
     </div>
