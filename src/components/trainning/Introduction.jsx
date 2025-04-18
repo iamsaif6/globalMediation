75376432daf0948db16e4ec6fd2c0faca9862cec
Saif Hasan <saifhassan399@gmail.com>
2025-04-18 14:57:26 +0600

    Purple and blue wash remove over block images

--- src/components/trainning/Introduction.jsx
+++ src/components/trainning/Introduction.jsx
@@ -1,9 +1,26 @@
+'use client';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import service1 from '/public/Training 1.png';
 import Button from '../shared/Button';
+import Link from 'next/link';
+import Modal from 'react-modal';
 
 const Introduction = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  function openModal() {
+    setModalOpen(true);
+  }
+
+  function closeModal() {
+    setModalOpen(false);
+  }
+
+  function afterCloseModal() {
+    closeModal();
+  }
+
   return (
     <section>
       <div className=" py-16 px-5 md:px-16 md:py-[80px] items-center grid grid-cols-2 gap-[80px]">
@@ -38,12 +55,109 @@ const Introduction = () => {
                 <span>Comprehensive support, including tutorials, coaching and feedback</span>
               </li>
             </ul>
+
             <div className="mt-6">
-              <Button title={'Start Your Mediation Journey Today'} href="/contact-us" />
+              <button
+                onClick={openModal}
+                className={`bg-primary border cursor-pointer border-primary font-normal text-lg text-[#FCFCFD] px-[42px] py-3 rounded-[40px]`}
+              >
+                Start Your Mediation Journey Today
+              </button>
             </div>
           </div>
         </div>
       </div>
+
+      {/* Modal */}
+      <Modal isOpen={modalOpen} onRequestClose={afterCloseModal} contentLabel="Mediation Savings Results">
+        <div className="p-0 border-0">
+          <div className="navbar flex items-center justify-end flex-wrap">
+            {/* Delete and Close Modal Section */}
+            <div className="buttons flex items-center gap-3 !mt-0 px-2">
+              <button
+                onClick={() => closeModal()}
+                className="close cursor-pointer text-sm text-[#17181B] bg-transparent h-7 w-7 flex items-center justify-center rounded-full transition-all hover:bg-gray-200"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <div className="mt-8">
+              <h2 className="text-4xl text-center font-semibold mb-5 opacity-80">Get in Touch</h2>
+              <div className=" leading-[140%]">
+                <form className="md:space-y-4  rounded-3xl py-10 px-8 space-y-6 ">
+                  {/* <div className="grid grid-cols-2 gap-5"></div> */}
+                  <div>
+                    <label className="text-base font-normal md:text-[16px] block mb-2.5 text-[#393D4E]" htmlFor="name">
+                      Name
+                    </label>
+                    <input className="w-full outline-0  p-3 rounded-xl bg-[#F8F8F8]" type="text" name="name" id="name" />
+                  </div>
+                  <div>
+                    <label className="text-base font-normal md:text-[16px] block mb-2.5 text-[#393D4E]" htmlFor="email">
+                      Email
+                    </label>
+                    <input required className="w-full outline-0 p-3 rounded-xl bg-[#F8F8F8]" type="email" name="email" id="email" />
+                  </div>
+                  <div>
+                    <label className="text-base font-normal md:text-[16px] block mb-2.5 text-[#393D4E]" htmlFor="company">
+                      Company name
+                    </label>
+                    <input className="w-full outline-0 p-3 rounded-xl bg-[#F8F8F8]" type="text" name="company" id="company" />
+                  </div>
+                  <div>
+                    <label className="text-base font-normal md:text-[16px] block mb-2.5 text-[#393D4E]" htmlFor="conflict">
+                      Type of conflict
+                    </label>
+                    <input className="w-full outline-0 p-3 rounded-xl bg-[#F8F8F8]" type="text" name="conflict" id="conflict" />
+                  </div>
+                  <div>
+                    <label className="text-base font-normal md:text-[16px] block mb-2.5 text-[#393D4E]" htmlFor="conflict">
+                      Message
+                    </label>
+                    <textarea
+                      placeholder="Type your message..."
+                      className="w-full outline-0 placeholder:text-[20px] placeholder:text-[#C5C0C9] p-3 rounded-xl bg-[#F8F8F8]"
+                      type="text"
+                      name="conflict"
+                      id="conflict"
+                      rows={3}
+                    />
+                  </div>
+                  <div className="flex gap-1.5 items-center">
+                    <input required type="checkbox" />
+                    <label className="text-base font-normal md:text-[16px] block  text-[#393D4E]" htmlFor="conflict">
+                      I accept the{' '}
+                      <Link href={'#'} className="underline">
+                        Terms
+                      </Link>
+                    </label>
+                  </div>
+                  <button className="bg-primary cursor-pointer mt-3 py-3 px-8 text-white font-normal text-lg rounded-3xl" type="submit">
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+            {/* 
+            <div className="md:flex hidden items-center gap-4 justify-center mt-[60px]">
+              <Button title={'Schedule a Mediation Session'} href="/contact-us" />
+              <button className="font-normal border border-[#393D4E] text-lg text-[#393D4E] px-[24px] py-3 rounded-[40px]">
+                <Link className="w-full" href="/contact-us">
+                  Speak to a Mediator Today
+                </Link>
+              </button>
+            </div> */}
+          </div>
+        </div>
+      </Modal>
     </section>
   );
 };
