dc73d7efb1c14115cd69c5e2c524477c16af485d
Saif Hasan <saifhassan399@gmail.com>
2025-10-17 11:58:55 +0600

    Update - homepage card hidden

--- src/app/page.js
+++ src/app/page.js
@@ -29,11 +29,12 @@ export default function Home() {
       <div id="root">
         <Header />
         <Marque />
-        <MediationCards />
+        {/* <MediationCards /> */}
         {/*Start Learn About Mediation */}
+        {/* my-[64px] md:my-[80px] */}
         <section
           aria-label="A Better Way to Resolve Disputes"
-          className="grid grid-cols-2 lg:max-w-[1318px] mx-auto gap-4 my-[64px] md:my-[80px] px-5 md:px-[60px]"
+          className="grid grid-cols-2 lg:max-w-[1318px] mx-auto gap-4 mb-[64px] md:mb-[80px]  px-5 md:px-[60px]"
         >
           <div className="col-span-2 lg:col-span-1 text-white bg-primary rounded-xl lg:rounded-3xl px-6 lg:px-[58px] py-[34px] lg:py-[61px]">
             <h2 className="font-medium leading-[120%] text-[32px] lg:text-5xl mb-[18px]">A better way to resolve disputes</h2>
